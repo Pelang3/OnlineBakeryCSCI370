21ad58ef2411b28125713b31b3d1a7c5c2727e09
roseleen-kaur <106797056+roseleen-kaur@users.noreply.github.com>
2022-11-19 19:45:12 -0500

    Modified Factory Cake class

--- src/Factory/Cake.java
+++ src/Factory/Cake.java
@@ -1,21 +1,29 @@
 package Factory;
+import java.util.ArrayList;
 
-public class Cake implements IItem{
-        private int Price = 2;
-        private int size = 0;
-        public Cake(int Size){
-            this.size = Size;
-
-            if(size == 6){
-                Price = 20;
-            }
-            if(size == 9){
-                Price = 25;
-            }
-        }
-        @Override
-        public int getPrice() {
-            // TODO Auto-generated method stub
-            return Price;
-        }
+public class Cake implements IItem {
+	private int Price = 2;
+	private int Size = 2;
+	public ArrayList<String> toppings;
+	
+	public Cake(int size) {
+		toppings = new ArrayList<>();
+		
+		if(size == 6){
+			Price = 20;
+		}
+		
+		if(size == 9) {
+			Price = 25;
+		}
+	}
+	
+	@Override
+	public int getPrice() {
+		return Price;
+	}
+	
+	public int getSize() {
+		return Size;
+	}
 }
