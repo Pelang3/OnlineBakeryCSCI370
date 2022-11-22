9a3408b357c41b730b6bba87c31301af5580922f
roseleen-kaur <106797056+roseleen-kaur@users.noreply.github.com>
2022-11-21 22:32:51 -0500

    Updates for controller

--- src/Decorator/IcingDecorator.java
+++ src/Decorator/IcingDecorator.java
@@ -3,14 +3,16 @@ package Decorator;
 import Factory.Cake;
 
 public class IcingDecorator extends CakeDecorator {
-	
+	String decorator = "Icing";
 	
 	public IcingDecorator(Cake baseCake) {
 		super(baseCake);
+		if (!toppings.contains(decorator))
+			decorate();
 	}
 	
 	public void decorate() {
-		baseCake.toppings.add("Icing");
+		baseCake.toppings.add(decorator);
 	}
 	
 }
