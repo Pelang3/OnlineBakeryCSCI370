be0737af34947990b5b3222da5eab3747db282df
roseleen-kaur <106797056+roseleen-kaur@users.noreply.github.com>
2022-11-21 22:33:21 -0500

    Updates for controller

--- src/Decorator/SprinkleDecorator.java
+++ src/Decorator/SprinkleDecorator.java
@@ -3,14 +3,16 @@ package Decorator;
 import Factory.Cake;
 
 public class SprinkleDecorator extends CakeDecorator {
-	
+	String decorator = "Sprinkles";
 	
 	public SprinkleDecorator(Cake baseCake) {
 		super(baseCake);
+		if (!toppings.contains(decorator))
+			decorate();
 	}
 	
 	public void decorate() {
-		baseCake.toppings.add("Sprinkles");
+		baseCake.toppings.add(decorator);
 	}
 	
 }
