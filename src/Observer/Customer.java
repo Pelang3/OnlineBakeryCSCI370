2190396f1c0e5f392333e16d0975fd647323b74f
Pedro Araujo <78515402+PAraujoo@users.noreply.github.com>
2022-11-26 01:36:03 -0500

    Update Customer.java

--- src/Observer/Customer.java
+++ src/Observer/Customer.java
@@ -1,17 +1,17 @@
 package Observer;
 public class Customer implements Observer {
 
-	private String name; // <-- 1.) Potentially modified later
+	private String name; 
 	
 	
-	public Customer(String name) // <-- 2.) Potentially modified later
+	public Customer(String name) 
 	{
 		this.name = name;
 	}
 	
 	public void update(int discount) 
 	{
-		System.out.println("Hello " + name +"! " + discount + "% discount available for you!");
+		System.out.println("Congrats " + name +"! " + discount + "% discount available for you!");
 	}
 	
 	
