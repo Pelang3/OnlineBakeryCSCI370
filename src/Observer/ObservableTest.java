468988a68f9fc177278e9f3d9b96d245b0dc3a86
Pedro Araujo <78515402+PAraujoo@users.noreply.github.com>
2022-11-26 23:23:49 -0500

    Add files via upload

---
+++ src/Observer/ObservableTest.java
@@ -0,0 +1,53 @@
+//package Observer;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ObservableTest {
+    private StoreObservable onlineBakery = new StoreObservable();
+    @org.junit.jupiter.api.Test
+    void observableDiscount()
+    {
+        onlineBakery.setDiscount(10);
+        onlineBakery.setDiscount(20);
+        assertTrue(onlineBakery.getDiscount() != 10);
+    }
+
+    @org.junit.jupiter.api.Test
+    void observableSubscriberCount()
+    {
+        Customer c1 = new Customer("Pedro");
+        Customer c2 = new Customer("Ben");
+        Customer c3 = new Customer("Will");
+        
+        onlineBakery.addSubscriber(c1);
+        onlineBakery.addSubscriber(c2);
+        onlineBakery.addSubscriber(c3);
+
+        assertTrue(onlineBakery.size() == 3);
+    }
+
+    @org.junit.jupiter.api.Test
+    void observableAccurateSub()
+    {
+        Customer c1 = new Customer("Pedro");
+        Customer c2 = new Customer("Ben");
+        Customer c3 = new Customer("Will");
+        
+        onlineBakery.addSubscriber(c1);
+        onlineBakery.addSubscriber(c2);
+        onlineBakery.addSubscriber(c3);
+
+        onlineBakery.removeSubscriber(c1);
+
+        assertFalse(!onlineBakery.contains(c3), "Effective");
+    }
+}
+/*
+ * Component Test: 
+ * 5 users subscribe to the online bakery newsletter to receive
+ * discounts.
+ * The size of the subscriber count should be 5.
+ * When the online bakery sets the discount to (for example) 15%
+ * that day, a user who subcribes to the newsletter and gets
+ * lucky, a message will display informing the user they have
+ * received an exclusive 15% discount.
+ */
